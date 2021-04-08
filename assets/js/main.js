07f31ae1e58f5e4140631e55ad9bf59e77dedbbe
Nghi <phamtonghicoder96@gmail.com>
2021-04-08 17:30:10 +0700

    feat(main.js) add animation
    add count auto for funfact section
    add class active when scroll and click on nav
    
    Close

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,23 @@ function hamburgerFunction() {
     var x = document.getElementById("my__navbar");
     if (x.className === "inner__collapse") {
         x.className += " show";
-    } else {
+    }
+    else {
         x.className = "inner__collapse";
     }
 }
 
-
-// Show class active when click
-var header = document.getElementById("my__navbar");
-var links = header.getElementsByClassName("nav__link");
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
+//
+// // Add/delete class active when click on header
+// var header = document.getElementById("my__navbar");
+// var links = header.getElementsByClassName("nav__link");
+// for (var i = 0; i < links.length; i++) {
+//     links[i].addEventListener("click", function() {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
 
 // Slick for Banner
 $('.banner').slick({
@@ -41,7 +42,6 @@ $('.features__slick').slick({
 });
 
 //Slick for Team
-
 $('.team__slick').slick({
     dots: true,
     infinite: true,
@@ -50,5 +50,79 @@ $('.team__slick').slick({
     arrows: false,
     cssEase: 'linear',
 });
+
+//Count to for Fun Fact
+$(window).on("load resize",function() {
+
+    var counters = $(".timer");
+    var countersQuantity = counters.length;
+    var counter = [];
+
+    for (i = 0; i < countersQuantity; i++) {
+        counter[i] = parseInt(counters[i].innerHTML);
+    }
+
+    var count = function(start, value, id) {
+        var localStart = start;
+        setInterval(function() {
+            if (localStart < value) {
+                localStart += 2;
+                counters[id].innerHTML = localStart;
+            }
+        }, 4);
+    }
+
+    for (j = 0; j < countersQuantity; j++) {
+        count(0, counter[j], j);
+    }
+});
+
+// Add/delete class active when scroll screen
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 10, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+
+            //change background color for nav when scroll
+            var nav = document.getElementById("nav");
+            if (e.target.innerHTML !== "Home") {
+                console.log(e.target.innerHTML);
+                nav.setAttribute('style', 'background-color: $color-background-header');
+            }
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#my__navbar a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if ((refElement.position().top - 70) <= scrollPos && (refElement.position().top - 70 + refElement.height()) > scrollPos) {
+            $('#my__navbar a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
 
 
