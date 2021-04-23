fc4a5601da1546df70183df73f56ae40e3d1f0cb
Nghi <phamtonghicoder96@gmail.com>
2021-04-23 14:50:09 +0700

    fix(all page) change size (margin, padding, font-size, line_height), change color text, add active for _work
    change line-height for _features, address of _discuss
    change color-text for _features, nav link  of _works
    add active for nav of _works
    
    Close

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,25 @@ $(function() {
         //End if
     });
 
+    $(".links__item").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            $("a").each(function () {
+                $(this).removeClass('active');
+            });
+
+            $(this).addClass('active');
+        }
+        //End if
+    });
+
     /**
      * Event move to new section
      */
